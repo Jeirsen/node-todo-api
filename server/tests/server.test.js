a1d91f22263dafbd37ea65f80f9e6b0558a71105
Jeirsen <jeirsen@gmail.com>
2019-02-19 16:07:29 -0500

    add test cases for get todo by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,8 +2,13 @@ const expect = require("expect");
 const request = require("supertest");
 const { app } = require("../server");
 const { Todo } = require("../models/todo");
+const { ObjectID } = require("mongodb");
 
-const todos = [{ text: "React.js" }, { text: "Angular" }, { text: "Node.js" }];
+const todos = [
+  { _id: new ObjectID(), text: "React.js" },
+  { _id: new ObjectID(), text: "Angular" },
+  { _id: new ObjectID(), text: "Node.js" }
+];
 
 beforeEach(done => {
   Todo.remove({})
@@ -71,6 +76,34 @@ describe("GET /todos", () => {
       .expect(res => {
         expect(res.body.todos.length).toBe(3);
       })
+      .end(done);
+  });
+});
+
+describe("GET /todos/:id", () => {
+  it("should return single todo doc", done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it("should return 404 for todo not found", done => {
+    id = new ObjectID();
+    request(app)
+      .get(`/todos/${id.toHexString()}`)
+      .expect(400)
+      .end(done);
+  });
+
+  it("should return 404 for ivalid id format", done => {
+    let invalirId = "5c6b8669bf92b642ea0223a0111";
+    request(app)
+      .get(`/todos/${invalirId}`)
+      .expect(400)
       .end(done);
   });
 });
