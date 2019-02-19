4299f5185f1822b0f26145cd639f612b811a7dd0
Jeirsen <jeirsen@gmail.com>
2019-02-18 19:27:26 -0500

    refactor server api, add todos route

---
+++ server/server.js
@@ -0,0 +1,31 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+
+let { mongoose } = require("./db/mongoose.js");
+let { Todo } = require("./models/todo.js");
+let { User } = require("./models/user.js");
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send({
+        error: "Error trying to create the todo"
+      });
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server up on port 3000");
+});
