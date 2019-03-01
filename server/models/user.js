912e9e2f0f1860584ea1c96747d77b282b13b690
Jeirsen <jeirsen@gmail.com>
2019-02-28 22:01:33 -0500

    added new files js

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,37 @@
-let mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-let User = mongoose.model("User", {
+const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
+const User = mongoose.model("User", userSchema);
 
 module.exports = {
   User
